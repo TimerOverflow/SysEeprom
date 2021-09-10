fd8ef6622c0d242d77be7f6449bb8db6b5747582
TimerOverflow <48713974+TimerOverflow@users.noreply.github.com>
2021-09-10 15:28:38 +0900

    1. convert tab -> space

--- SysEeprom.c
+++ SysEeprom.c
@@ -16,256 +16,255 @@
 /*********************************************************************************/
 static inline tU8 CheckAllOfInit(tag_EepControl *Eep)
 {
-	return (Eep->Bit.InitGeneral) ? true : false;
+  return (Eep->Bit.InitGeneral) ? true : false;
 }
 /*********************************************************************************/
 static void EepWrite(tag_EepCommonConfig *EepConfig, tU16 Addr, tU8 Data)
 {
-	tU8 Cnt = 0;
-	
-	while(EepConfig->HalEepromWrite(Addr, Data) == false)
-	{
-		if(++Cnt >= 5){ EepConfig->Bit.WriteFail = true; return; }
-	}
+  tU8 Cnt = 0;
+  
+  while(EepConfig->HalEepromWrite(Addr, Data) == false)
+  {
+    if(++Cnt >= 5){ EepConfig->Bit.WriteFail = true; return; }
+  }
 }
 /*********************************************************************************/
 static void EepRead(tag_EepCommonConfig *EepConfig, tU16 Addr, tU8 *Data)
 {
-	tU8 Cnt = 0;
-	
-	while(EepConfig->HalEepromRead(Addr, Data) == false)
-	{
-		if(++Cnt >= 5){ EepConfig->Bit.ReadFail = true; return; }
-	}
+  tU8 Cnt = 0;
+  
+  while(EepConfig->HalEepromRead(Addr, Data) == false)
+  {
+    if(++Cnt >= 5){ EepConfig->Bit.ReadFail = true; return; }
+  }
 }
 /*********************************************************************************/
 tU8 InitEepCommonConfig(tag_EepCommonConfig *EepConfig, tU16 LastAddr, tU8 (*HalEepromWrite)(tU16 Addr, tU8 Data), tU8 (*HalEepromRead)(tU16 Addr, tU8 *pData))
 {
-	tU16 *pLastAddr = (tU16 *) &EepConfig->LastAddr;
-	static tag_EepControl EepSignature = { .Bit.InitGeneral = false };
-	volatile static tU8 StrSignature[3];
+  tU16 *pLastAddr = (tU16 *) &EepConfig->LastAddr;
+  static tag_EepControl EepSignature = { .Bit.InitGeneral = false };
+  volatile static tU8 StrSignature[3];
 
-	/*
-		1) 인수
-			- EepConfig : EepConfig 인스턴스의 주소
-			- HalEepromWrite : 사용자가 구현한 HalEepromWrite 함수 포인터 주소
-			- HalEepromRead : 사용자가 구현한 HalEepromRead 함수 포인터 주소
+  /*
+    1) 인수
+      - EepConfig : EepConfig 인스턴스의 주소
+      - HalEepromWrite : 사용자가 구현한 HalEepromWrite 함수 포인터 주소
+      - HalEepromRead : 사용자가 구현한 HalEepromRead 함수 포인터 주소
 
-		2) 반환
-			- 초기화 성공 여부
+    2) 반환
+      - 초기화 성공 여부
 
-		3) 설명
-			- tag_EepControl::Config 구조체의 필요한 정보를 초기화 한다.
-			- EEPROM의 크기, HAL 구현 함수등을 연결.
-	*/
+    3) 설명
+      - tag_EepControl::Config 구조체의 필요한 정보를 초기화 한다.
+      - EEPROM의 크기, HAL 구현 함수등을 연결.
+  */
 
-	*pLastAddr = LastAddr;
-	EepConfig->HalEepromWrite = HalEepromWrite;
-	EepConfig->HalEepromRead = HalEepromRead;
-	EepConfig->AllocEepAddr = 0;
-	EepConfig->Bit.Init = true;
-	
-	memset((void *) StrSignature, 0, sizeof(StrSignature));
-	
-	InitEepControl(&EepSignature, (const tU8 *) StrSignature, sizeof(StrSignature), EepConfig);
-	DoEepReadControl(&EepSignature);
-	if((StrSignature[0] != 'J') || (StrSignature[1] != 'H') || (StrSignature[2] != 'G'))
-	{
-		DoEepReadControl(&EepSignature);
-		if((StrSignature[0] != 'J') || (StrSignature[1] != 'H') || (StrSignature[2] != 'G'))
-		{
-			EepConfig->Bit.FirstExecute = true;
-		}
-	}
-	
-	return EepConfig->Bit.Init;
+  *pLastAddr = LastAddr;
+  EepConfig->HalEepromWrite = HalEepromWrite;
+  EepConfig->HalEepromRead = HalEepromRead;
+  EepConfig->AllocEepAddr = 0;
+  EepConfig->Bit.Init = true;
+  
+  memset((void *) StrSignature, 0, sizeof(StrSignature));
+  
+  InitEepControl(&EepSignature, (const tU8 *) StrSignature, sizeof(StrSignature), EepConfig);
+  DoEepReadControl(&EepSignature);
+  if((StrSignature[0] != 'J') || (StrSignature[1] != 'H') || (StrSignature[2] != 'G'))
+  {
+    DoEepReadControl(&EepSignature);
+    if((StrSignature[0] != 'J') || (StrSignature[1] != 'H') || (StrSignature[2] != 'G'))
+    {
+      EepConfig->Bit.FirstExecute = true;
+    }
+  }
+  
+  return EepConfig->Bit.Init;
 }
 /*********************************************************************************/
 tU8 InitEepControl(tag_EepControl *Eep, const tU8 *DataBase, tU16 Length, tag_EepCommonConfig *Config)
 {
-	const tU8 GapOfAnotherSector = 10;
-	tU8 **pDataBase = (tU8 **) &Eep->DataBase;
-	tU16 *pEepBase = (tU16 *) &Eep->EepBase;
-	tU16 *pLength = (tU16 *) &Eep->Length;
-	tag_EepBitField *pBit = (tag_EepBitField *) &Eep->Bit;
-	tag_EepCommonConfig **pConfig = (tag_EepCommonConfig **) &Eep->Config;
+  const tU8 GapOfAnotherSector = 10;
+  tU8 **pDataBase = (tU8 **) &Eep->DataBase;
+  tU16 *pEepBase = (tU16 *) &Eep->EepBase;
+  tU16 *pLength = (tU16 *) &Eep->Length;
+  tag_EepBitField *pBit = (tag_EepBitField *) &Eep->Bit;
+  tag_EepCommonConfig **pConfig = (tag_EepCommonConfig **) &Eep->Config;
 
-	/*
-		1) 인수
-			- Eep : tag_EepControl 인스턴스의 주소
-			- DataBase : eeprom을 통해 관리할 대상의 시작주소
-			- Length : 관리할 대상의 크기
-			- Config : Eeprom에 대한 크기 정보, HAL 함수 정보를 가지고 있는 tag_EepCommonConfig 구조체 인스턴스의 주소.
+  /*
+    1) 인수
+      - Eep : tag_EepControl 인스턴스의 주소
+      - DataBase : eeprom을 통해 관리할 대상의 시작주소
+      - Length : 관리할 대상의 크기
+      - Config : Eeprom에 대한 크기 정보, HAL 함수 정보를 가지고 있는 tag_EepCommonConfig 구조체 인스턴스의 주소.
 
-		2) 반환
-			- 초기화 성공 여부
+    2) 반환
+      - 초기화 성공 여부
 
-		3) 설명
-			- tag_EepControl 인스턴스를 초기화한다.
-			- tag_EepControl 인스턴스에 eeprom으로 부터 읽거나 쓸 대상의 주소와, 할당할 eeprom의 시작과 길이를 초기화한다.
-	*/
+    3) 설명
+      - tag_EepControl 인스턴스를 초기화한다.
+      - tag_EepControl 인스턴스에 eeprom으로 부터 읽거나 쓸 대상의 주소와, 할당할 eeprom의 시작과 길이를 초기화한다.
+  */
 
-	if(Config->Bit.Init == false)
-	{
-		return false;
-	}
+  if(Config->Bit.Init == false)
+  {
+    return false;
+  }
 
-	*pConfig = (tag_EepCommonConfig *) Config;
-	//Link with "Eep" struct.
+  *pConfig = (tag_EepCommonConfig *) Config;
+  //Link with "Eep" struct.
 
-	if((DataBase != 0) && (Length != 0) && ((Config->AllocEepAddr + Length + GapOfAnotherSector) < Config->LastAddr))
-	{
-		*pLength = Length;
-		*pDataBase = (tU8 *) DataBase;
-		*pEepBase = Config->AllocEepAddr;
-		Config->AllocEepAddr += (Length + GapOfAnotherSector);
+  if((DataBase != 0) && (Length != 0) && ((Config->AllocEepAddr + Length + GapOfAnotherSector) < Config->LastAddr))
+  {
+    *pLength = Length;
+    *pDataBase = (tU8 *) DataBase;
+    *pEepBase = Config->AllocEepAddr;
+    Config->AllocEepAddr += (Length + GapOfAnotherSector);
 
-		pBit->InitGeneral = true;
-	}
+    pBit->InitGeneral = true;
+  }
 
-	pBit->InitComplete = CheckAllOfInit(Eep);
+  pBit->InitComplete = CheckAllOfInit(Eep);
 
-	return pBit->InitGeneral;
+  return pBit->InitGeneral;
 }
 /*********************************************************************************/
 void DoEepReadControl(tag_EepControl *Eep)
 {
-	tU8 *pDataBase = (tU8 *) Eep->DataBase;
-	tU16 Index = 0;
-	tU8 Data;
+  tU8 *pDataBase = (tU8 *) Eep->DataBase;
+  tU16 Index = 0;
+  tU8 Data;
 
-	/*
-		1) 인수
-			- Eep : tag_EepControl 인스턴스의 주소
+  /*
+    1) 인수
+      - Eep : tag_EepControl 인스턴스의 주소
 
-		2) 반환
-			- 없음.
+    2) 반환
+      - 없음.
 
-		3) 설명
-			- 해당 인스턴스의 eeprom으로 부터 데이터를 읽어와 대상 버퍼에 값을 대입.
-	*/
+    3) 설명
+      - 해당 인스턴스의 eeprom으로 부터 데이터를 읽어와 대상 버퍼에 값을 대입.
+  */
 
-	if((Eep->Bit.InitComplete == false) || (Eep->Config->Bit.ReadFail == true) || (Eep->Config->Bit.WriteFail == true))
-	{
-		return;
-		/* error or disabled */
-	}
+  if((Eep->Bit.InitComplete == false) || (Eep->Config->Bit.ReadFail == true) || (Eep->Config->Bit.WriteFail == true))
+  {
+    return;
+    /* error or disabled */
+  }
 
-	do
-	{
-		EepRead((tag_EepCommonConfig *) Eep->Config, Eep->EepBase + (Index), &Data);
-		pDataBase[Index] = Data;
-	}while(++Index < Eep->Length);
+  do
+  {
+    EepRead((tag_EepCommonConfig *) Eep->Config, Eep->EepBase + (Index), &Data);
+    pDataBase[Index] = Data;
+  }while(++Index < Eep->Length);
 }
 /*********************************************************************************/
 tU8 DoEepWriteControl(tag_EepControl *Eep)
 {
-	tU16 *pIndex = (tU16 *) &Eep->Index;
-	tag_EepCommonConfig *Config = (tag_EepCommonConfig *) Eep->Config;
-	tag_EepBitField *pBit;
-	tU8 Data = 0;
+  tU16 *pIndex = (tU16 *) &Eep->Index;
+  tag_EepCommonConfig *Config = (tag_EepCommonConfig *) Eep->Config;
+  tag_EepBitField *pBit;
+  tU8 Data = 0;
 
-	/*
-		1) 인수
-			- Eep : tag_EepControl 인스턴스의 주소
+  /*
+    1) 인수
+      - Eep : tag_EepControl 인스턴스의 주소
 
-		2) 반환
-			- 쓰기동작 중일 때 true, 그외 상황일 때 false.
+    2) 반환
+      - 쓰기동작 중일 때 true, 그외 상황일 때 false.
 
-		3) 설명
-			- 해당 인스턴스의 eeprom 영역에 관리할 대상 데이터를 쓰기를 진행한다.
-			- SetEepWriteEnable() 함수를 통해 'Eep.Bit.Write'를 활성화 시켜야 실행된다.
-			- 쓰기전 해당주소의 값이 쓰려고 하는 값과 일치하지 않을 경우에만 유효하다고 판단하며,
-				유효한 데이터만 쓰기를 진행하고 유효하지 않을 경우 다음 주소로 건너뛴다.
-			- 값이 유효하여 쓰기를 진행하거나 해당영역의 마지막에 도달할 경우에만 while loop를 탈출한다.
-	*/
+    3) 설명
+      - 해당 인스턴스의 eeprom 영역에 관리할 대상 데이터를 쓰기를 진행한다.
+      - SetEepWriteEnable() 함수를 통해 'Eep.Bit.Write'를 활성화 시켜야 실행된다.
+      - 쓰기전 해당주소의 값이 쓰려고 하는 값과 일치하지 않을 경우에만 유효하다고 판단하며,
+        유효한 데이터만 쓰기를 진행하고 유효하지 않을 경우 다음 주소로 건너뛴다.
+      - 값이 유효하여 쓰기를 진행하거나 해당영역의 마지막에 도달할 경우에만 while loop를 탈출한다.
+  */
 
-	if((Eep->Bit.InitComplete == false) || (Config->Bit.ReadFail == true) || (Config->Bit.WriteFail == true))
-	{
-		return false;
-		/* error or disabled */
-	}
-	
-	if(Eep->Bit.Write == true)
-	{
-		Config->SignatureWriteDelay = 5;
-		while(true)
-		{
-			EepRead(Config, Eep->EepBase + (*pIndex), &Data);
-			if(Data != Eep->DataBase[*pIndex])
-			{
-				EepWrite(Config, Eep->EepBase + (*pIndex), Eep->DataBase[*pIndex]);
-				return true;
-				/* check valid data */
-			}
-	
-			if(++(*pIndex) >= Eep->Length)
-			{
-				pBit = (tag_EepBitField *) &Eep->Bit;
-				pBit->Write = false;
-				return false;
-				/* end of sector */
-			}
-		}
-	}
-	else if(Config->Bit.FirstExecute)
-	{
-		if(Config->SignatureWriteDelay == 0){ EepWrite(Config, 0, 'J'); EepWrite(Config, 1, 'H'); EepWrite(Config, 2, 'G'); Config->Bit.FirstExecute = false; }
-		else{ Config->SignatureWriteDelay--; }
-	}
-	
-	return false;
+  if((Eep->Bit.InitComplete == false) || (Config->Bit.ReadFail == true) || (Config->Bit.WriteFail == true))
+  {
+    return false;
+    /* error or disabled */
+  }
+  
+  if(Eep->Bit.Write == true)
+  {
+    Config->SignatureWriteDelay = 5;
+    while(true)
+    {
+      EepRead(Config, Eep->EepBase + (*pIndex), &Data);
+      if(Data != Eep->DataBase[*pIndex])
+      {
+        EepWrite(Config, Eep->EepBase + (*pIndex), Eep->DataBase[*pIndex]);
+        return true;
+        /* check valid data */
+      }
+  
+      if(++(*pIndex) >= Eep->Length)
+      {
+        pBit = (tag_EepBitField *) &Eep->Bit;
+        pBit->Write = false;
+        return false;
+        /* end of sector */
+      }
+    }
+  }
+  else if(Config->Bit.FirstExecute)
+  {
+    if(Config->SignatureWriteDelay == 0){ EepWrite(Config, 0, 'J'); EepWrite(Config, 1, 'H'); EepWrite(Config, 2, 'G'); Config->Bit.FirstExecute = false; }
+    else{ Config->SignatureWriteDelay--; }
+  }
+  
+  return false;
 }
 /*********************************************************************************/
 void SetEepWriteEnable(tag_EepControl *Eep)
 {
-	/*
-		1) 인수
-			- Eep : tag_EepControl 인스턴스의 주소
+  /*
+    1) 인수
+      - Eep : tag_EepControl 인스턴스의 주소
 
-		2) 반환
-			- 없음.
+    2) 반환
+      - 없음.
 
-		3) 설명
-			- 해당 인스턴스에 쓰기 동작을 활성화한다.
-			- 'Eep.Bit.Write' 상태가 'true'일 때 DoEepWriteControl()함수가 쓰기 동작을 실행한다.
-	*/
+    3) 설명
+      - 해당 인스턴스에 쓰기 동작을 활성화한다.
+      - 'Eep.Bit.Write' 상태가 'true'일 때 DoEepWriteControl()함수가 쓰기 동작을 실행한다.
+  */
 
-	if(Eep->Bit.InitComplete == false)
-	{
-		return;
-		/* error or disabled */
-	}
+  if(Eep->Bit.InitComplete == false)
+  {
+    return;
+    /* error or disabled */
+  }
 
-	tag_EepBitField *pBit = (tag_EepBitField *) &Eep->Bit;
-	tU16 *pIndex = (tU16 *) &Eep->Index;
+  tag_EepBitField *pBit = (tag_EepBitField *) &Eep->Bit;
+  tU16 *pIndex = (tU16 *) &Eep->Index;
 
-	pBit->Write = true;
-	*pIndex = 0;
+  pBit->Write = true;
+  *pIndex = 0;
 }
 /*********************************************************************************/
 void EraseEepCommonConfigSignature(tag_EepCommonConfig *EepConfig)
 {
-	/*
-		1) 인수
-			- EepConfig : EepConfig 인스턴스의 주소
+  /*
+    1) 인수
+      - EepConfig : EepConfig 인스턴스의 주소
 
-		2) 반환
-			- 없음.
+    2) 반환
+      - 없음.
 
-		3) 설명
-			- EepConfig에서 최초 실행 여부를 확인하기 위한 참조 값인 Signature 값 삭제.
-			- 본 함수 실행 후 
-	*/
+    3) 설명
+      - EepConfig에서 최초 실행 여부를 확인하기 위한 참조 값인 Signature 값 삭제.
+      - 본 함수 실행 후 
+  */
 
-	if((EepConfig->Bit.Init == false) || (EepConfig->Bit.ReadFail == true) || (EepConfig->Bit.WriteFail == true))
-	{
-		return;
-	}
-	
-	EepWrite(EepConfig, 0, 0);
-	EepWrite(EepConfig, 1, 0);
-	EepWrite(EepConfig, 2, 0);
+  if((EepConfig->Bit.Init == false) || (EepConfig->Bit.ReadFail == true) || (EepConfig->Bit.WriteFail == true))
+  {
+    return;
+  }
+  
+  EepWrite(EepConfig, 0, 0);
+  EepWrite(EepConfig, 1, 0);
+  EepWrite(EepConfig, 2, 0);
 }
 /*********************************************************************************/
-
--- SysEeprom.h
+++ SysEeprom.h
@@ -5,64 +5,64 @@
 */
 /*********************************************************************************/
 #ifndef __SYS_EEPROM_H__
-#define	__SYS_EEPROM_H__
+#define  __SYS_EEPROM_H__
 /*********************************************************************************/
 #include "SysTypedef.h"
 /*********************************************************************************/
-#define SYS_EEPROM_REVISION_DATE		20200417
+#define SYS_EEPROM_REVISION_DATE    20200417
 /*********************************************************************************/
 /** REVISION HISTORY **/
 /*
-	2020. 04. 17.					- tag_EepCommonConfig의 시그니처는 연결된 모든 tag_EepControl 인스턴스들이
-	Jeong Hyun Gu						write동작을 끝냈을 때 write하도록 변경.
+  2020. 04. 17.          - tag_EepCommonConfig의 시그니처는 연결된 모든 tag_EepControl 인스턴스들이
+  Jeong Hyun Gu            write동작을 끝냈을 때 write하도록 변경.
 
-	2020. 01. 15.					- 실제 Eeprom과의 정상 동작 여부 확인 기능 추가.
-	Jeong Hyun Gu					- CheckEepromError() 함수 추가. 'tag_EepCommonConfig'에서 관리 하는 실제
-													Eeprom의 정상 동작 여부 확인.
+  2020. 01. 15.          - 실제 Eeprom과의 정상 동작 여부 확인 기능 추가.
+  Jeong Hyun Gu          - CheckEepromError() 함수 추가. 'tag_EepCommonConfig'에서 관리 하는 실제
+                          Eeprom의 정상 동작 여부 확인.
 
-	2019. 10. 01.					- EraseEepCommonConfigSignature() 함수 추가. InitEepCommonConfig()에서
-	Jeong Hyun Gu						최초 실행 여부를 확인하는데 사용하는 참조 값 Signature를 지워 다음 실행에서
-													공장 초기화 실행 가능.
+  2019. 10. 01.          - EraseEepCommonConfigSignature() 함수 추가. InitEepCommonConfig()에서
+  Jeong Hyun Gu            최초 실행 여부를 확인하는데 사용하는 참조 값 Signature를 지워 다음 실행에서
+                          공장 초기화 실행 가능.
 
-	2019. 09. 24.					- CheckEepromFirstExecuteSignature() 매크로 함수 추가. 직접 비트 필드에
-	Jeong Hyun Gu						접근하지 않고 위 매크로 함수로 접근.
+  2019. 09. 24.          - CheckEepromFirstExecuteSignature() 매크로 함수 추가. 직접 비트 필드에
+  Jeong Hyun Gu            접근하지 않고 위 매크로 함수로 접근.
 
-	2019. 09. 05.					- 프로그램 최초 실행 감지 루틴을 모듈 내 InitEepCommonConfig() 함수에 추가.
-	Jeong Hyun Gu						이제 어플리케이션에서 시그니쳐 확인하여 초기화를 실행 여부를 판단할 필요없이
-													tag_EepCommonConfig::Bit.FirstExecute 비트가 '1'이면 최초 실행으로 판단.
-												- 이전 버전에서 업데이트 시 AVR-EESAVE 불가.
+  2019. 09. 05.          - 프로그램 최초 실행 감지 루틴을 모듈 내 InitEepCommonConfig() 함수에 추가.
+  Jeong Hyun Gu            이제 어플리케이션에서 시그니쳐 확인하여 초기화를 실행 여부를 판단할 필요없이
+                          tag_EepCommonConfig::Bit.FirstExecute 비트가 '1'이면 최초 실행으로 판단.
+                        - 이전 버전에서 업데이트 시 AVR-EESAVE 불가.
 
-	2019. 07. 23.					- Eeprom_Write(), Eeprom_Read() 삭제하고 HAL 적용.
-	Jeong Hyun Gu					- 접두어 AVR -> SYS로 변경.
-												- GetDataFromEeprom() 삭제.
-												- "tag_EepCommonConfig" 추가.
+  2019. 07. 23.          - Eeprom_Write(), Eeprom_Read() 삭제하고 HAL 적용.
+  Jeong Hyun Gu          - 접두어 AVR -> SYS로 변경.
+                        - GetDataFromEeprom() 삭제.
+                        - "tag_EepCommonConfig" 추가.
 
-	2019. 01. 14.					- CPU타입 __AVR_ATMEGA2560__ 추가.
-	Jeong Hyun Gu
+  2019. 01. 14.          - CPU타입 __AVR_ATMEGA2560__ 추가.
+  Jeong Hyun Gu
 
-	2018. 09. 21.					- DoEepWriteControl() 함수에서 Length 보다 1바이트 더 쓰던 현상 수정.
-	Jeong Hyun Gu
+  2018. 09. 21.          - DoEepWriteControl() 함수에서 Length 보다 1바이트 더 쓰던 현상 수정.
+  Jeong Hyun Gu
 
-	2016. 12. 26.					- Eeprom_Write(), Eeprom_Read() 함수에서 WDT RESET 추가.
-	Jeong Hyun Gu
+  2016. 12. 26.          - Eeprom_Write(), Eeprom_Read() 함수에서 WDT RESET 추가.
+  Jeong Hyun Gu
 
-	2016. 11. 30.					- DoEepReadControl(), GetDataFromEeprom() 함수에서 Length 보다 1바이트 더 읽던 현상 수정.
-	Jeong Hyun Gu
+  2016. 11. 30.          - DoEepReadControl(), GetDataFromEeprom() 함수에서 Length 보다 1바이트 더 읽던 현상 수정.
+  Jeong Hyun Gu
 
-	2016. 11. 08.					- revision valid check 추가.
-	Jeong Hyun Gu
+  2016. 11. 08.          - revision valid check 추가.
+  Jeong Hyun Gu
 
-	2016. 10. 28.					- DoEepWriteControl() 반환값 수정.
-	Jeong Hyun Gu					- 반환값 관련 주석 추가.
+  2016. 10. 28.          - DoEepWriteControl() 반환값 수정.
+  Jeong Hyun Gu          - 반환값 관련 주석 추가.
 
-	2016. 10. 28.					- 초기버전.
-	Jeong Hyun Gu
+  2016. 10. 28.          - 초기버전.
+  Jeong Hyun Gu
 */
 /*********************************************************************************/
 /**Define**/
 
-#define	true		1
-#define	false		0
+#define  true     1
+#define  false    0
 
 /*********************************************************************************/
 /**Enum**/
@@ -73,39 +73,39 @@
 
 typedef struct
 {
-	struct
-	{
-		tU8 Init						:		1;
-		tU8 FirstExecute		:		1;
-		tU8 ReadFail				:		1;
-		tU8 WriteFail				:		1;
-	}Bit;
+  struct
+  {
+    tU8 Init            :    1;
+    tU8 FirstExecute    :    1;
+    tU8 ReadFail        :    1;
+    tU8 WriteFail       :    1;
+  }Bit;
 
-	const tU16 LastAddr;					// eeprom마지막 주소(크기)
-	tU16 AllocEepAddr;
-	tU8 SignatureWriteDelay;
+  const tU16 LastAddr;          // eeprom마지막 주소(크기)
+  tU16 AllocEepAddr;
+  tU8 SignatureWriteDelay;
 
-	tU8 (*HalEepromWrite)(tU16 Addr, tU8 Data);
-	tU8 (*HalEepromRead)(tU16 Addr, tU8 *pData);
+  tU8 (*HalEepromWrite)(tU16 Addr, tU8 Data);
+  tU8 (*HalEepromRead)(tU16 Addr, tU8 *pData);
 }tag_EepCommonConfig;
 
 typedef struct
 {
-	tU8 InitGeneral			:		1;
-	tU8 InitComplete		:		1;
-	tU8 Write						:		1;
+  tU8 InitGeneral      :    1;
+  tU8 InitComplete     :    1;
+  tU8 Write            :    1;
 }tag_EepBitField;
 
 typedef struct
 {
-	const tag_EepBitField Bit;
-	const tag_EepCommonConfig *Config;
+  const tag_EepBitField Bit;
+  const tag_EepCommonConfig *Config;
 
-	const tU16 EepBase; 					// eeprom의 시작 주소
-	const tU8 *DataBase;					// 관리할 데이터의 시작 주소
+  const tU16 EepBase;           // eeprom의 시작 주소
+  const tU8 *DataBase;          // 관리할 데이터의 시작 주소
 
-	const tU16 Index;	 					// 쓰기 인덱스
- 	const tU16 Length; 					// 관리할 데이터의 길이
+  const tU16 Index;             // 쓰기 인덱스
+  const tU16 Length;            // 관리할 데이터의 길이
 }tag_EepControl;
 
 /*********************************************************************************/
@@ -114,8 +114,8 @@ typedef struct
 tU8 InitEepCommonConfig(tag_EepCommonConfig *EepConfig, tU16 LastAddr, tU8 (*HalEepromWrite)(tU16 Addr, tU8 Data), tU8 (*HalEepromRead)(tU16 Addr, tU8 *pData));
 tU8 InitEepControl(tag_EepControl *Eep, const tU8 *DataBase, tU16 Length, tag_EepCommonConfig *EepConfig);
 
-#define CheckEepromFirstExecuteSignature(Eepconfig)					((Eepconfig)->Bit.FirstExecute)
-#define CheckEepromError(Eepconfig)					(((Eepconfig)->Bit.ReadFail) || ((Eepconfig)->Bit.WriteFail))
+#define CheckEepromFirstExecuteSignature(Eepconfig)          ((Eepconfig)->Bit.FirstExecute)
+#define CheckEepromError(Eepconfig)          (((Eepconfig)->Bit.ReadFail) || ((Eepconfig)->Bit.WriteFail))
 void EraseEepCommonConfigSignature(tag_EepCommonConfig *EepConfig);
 
 void SetEepWriteEnable(tag_EepControl *Eep);
